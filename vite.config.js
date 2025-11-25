598dcf98a34e090caffcc9032e8f695ec75bb4ec
Hashish03 <Khoasehashatsi40@gmail.com>
2025-11-25 15:06:48 +0200

    fix the reponame

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/k2mol-website/",
+  base: "/k2mol_website/",
   plugins: [react()],
 })
